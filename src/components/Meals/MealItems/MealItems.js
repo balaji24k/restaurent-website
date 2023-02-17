402c1bef4685a3adc9d81d0d33570c6aceab1860
balaji24k <balaji325s@gmail.com>
2023-02-17 20:36:51 +0530

    Added Form-Task4

--- src/components/Meals/MealItems/MealItems.js
+++ src/components/Meals/MealItems/MealItems.js
@@ -1,4 +1,5 @@
-import classes from './MealItems.module.css'
+import classes from './MealItems.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItems = (props) => {
     const price = `$${props.price.toFixed(2)}`;
@@ -10,7 +11,7 @@ const MealItems = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-
+                <MealItemForm/>
             </div>
 
         </li>
